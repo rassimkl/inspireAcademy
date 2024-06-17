7322682dc720f44fd41be03284373863f9a23b32
aligogo96 <33245901+alihayad@users.noreply.github.com>
2024-06-17 12:59:16 +0200

    allow spaces
    allow spaces when entering a phone number

--- app/Livewire/AddUser.php
+++ app/Livewire/AddUser.php
@@ -229,7 +229,7 @@ class AddUser extends Component
         'gender' => 'required|in:Male,Female',
         'date_of_birth' => 'nullable|date',
         'email' => 'required|email|unique:users,email',
-        'phone_number' => ['required', 'string', 'regex:/^\+?[0-9]+$/'],
+        'phone_number' => ['required', 'string', 'regex:/^\+?[0-9\s]+$/'],
         'blood_group' => 'nullable|string',
         'address' => 'nullable|string',
         'city' => 'nullable|string',
@@ -258,6 +258,7 @@ class AddUser extends Component
         // Create a new student record
         User::create($validatedData);
         $this->dispatch('resetSelect2');
+
 
 
 
