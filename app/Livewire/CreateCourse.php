1fb83310b195c4bc0abcedd852a01e72547ea322
aligogo96 <33245901+alihayad@users.noreply.github.com>
2024-06-28 14:55:23 +0200

    allow hours decimal

--- app/Livewire/CreateCourse.php
+++ app/Livewire/CreateCourse.php
@@ -34,7 +34,7 @@ class CreateCourse extends Component
                 'required',
                 Rule::in([1, 2]),
             ],
-            'totalHours' => 'required|numeric',
+            'totalHours' => 'required|regex:/^\d+(\.\d{1,2})?$/',
             'chargePerHour' => 'required|numeric',
             'teacher' => 'required|exists:users,id',
             'selectedStudents' => ['required', 'array'],
