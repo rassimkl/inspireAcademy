708713c9bcd147efcf71f774002a0c5251cb5084
aligogo96 <33245901+alihayad@users.noreply.github.com>
2024-07-03 16:24:57 +0200

    add indexes

---
+++ database/migrations/2024_07_03_162408_add_indexes_to_classes_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('classes', function (Blueprint $table) {
+            $table->index('course_id');
+            $table->index('date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('classes', function (Blueprint $table) {
+            $table->dropIndex(['course_id']);
+            $table->dropIndex(['date']);
+        });
+    }
+};
